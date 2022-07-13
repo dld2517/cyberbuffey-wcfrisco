063eab3bea9ede9241716673498d99ea095afb8b
Debian <debian@debian11.linuxvmimages.local>
2022-07-13 17:10:56 -0500

    day 3 working connections added code

--- python/guess.py
+++ python/guess.py
@@ -1,12 +1,22 @@
 #This is the crib drag attack
 
 key="The"
+key=key+".............................."
 # a=bytes.fromhex(input("Cipher Text 1: "))
 # b=bytes.fromhex(input("Cipher Text 2: "))
 a=bytes.fromhex("13070b4954491b53114816450d1b101c100000090952480105044e4b000f4b4e5453")
 b=bytes.fromhex("070d0a1e4c0c4e4d1d0f0d54431c10175311480a464604140a5b4e64373722233153")
-
+print("Loop through ciphertext a: ")
+print("A\tLet\tB")
+print(a)
+print(b)
+print(len(a))
+for by in a:
+    print(by,"/t",hex(by),"\t",str(a(by)),"\t")
 i=0
+print("CipherText Print by reference i")
+
+print(a[i],b[i])
 while(True):
     print()
     print("     0123456789012345678901234567890123456789")
@@ -14,7 +24,7 @@ while(True):
     print()
     print()
     plaintext = ""
-    for p in a:
+    for p in key:
         k= key[i]
         ik = ord(k)
         ip = p^ik
