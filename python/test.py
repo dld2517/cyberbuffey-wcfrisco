9929e5586752ec5be0fc9046b5c38addc08cf2f4
Danny Darden <dld2517@gmail.com>
2022-07-14 16:53:50 -0500

    updated test.py

--- python/test.py
+++ python/test.py
@@ -2,7 +2,9 @@ a=bytes.fromhex("13070b4954491b53114816450d1b101c100000090952480105044e4b000f4b4
 b=bytes.fromhex("070d0a1e4c0c4e4d1d0f0d54431c10175311480a464604140a5b4e64373722233153")
 print(a)
 print(b)
-print(len(a))
-for by in len(a):
-    print(by,"/t",hex(by),"\t",str(a(by)),"\t")
-i
+
+for by in range(len(a)):
+    print("Byte\tHex\tString\t\t")
+    print(by,"\t",hex(a[by]),"\t",str(a[by]),"\t")
+
+
